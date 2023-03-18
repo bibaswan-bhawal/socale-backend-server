701ffbe302699010e4ded6da17fa493f118b1c27
GBarrosKing <124916230+GBarrosKing@users.noreply.github.com>
2023-03-18 11:36:16 -0700

    added onboarding college quotes for UCI, Berkely and UCLA

---
+++ surreal/data/uni_quotes.js
@@ -0,0 +1,42 @@
+export const uniQuotes = {
+    "ucsd" : [
+
+    ],
+    "berkely" : [
+        `Standing at 307 feet tall,
+        the Campanile is the third 
+        tallest bell and clock tower in the world.`,
+
+        `The University Botanical Garden has over 10,000 types of plants
+        including many rare and endangered species.`,
+
+        `at Berkely there are over 1000 student groups.
+        Theres something for everyone at Berkely.`
+    ],
+    'ucla' : [
+        `The internet was invented at UCLA.
+        The first transmission was sent to
+        Stanford in 1969.`,
+
+        `UCLA students once covered parts
+        of the hollywood sign to say
+        \"GO BRUINS\".`,
+
+        `UCLA was founded in 1919.
+        It is the only leading research institution in the U.S.
+        that was started in the 20th century.`
+    ],
+    'uci' : [
+        `UCI is ranked among the best colleges
+        for students with learning disabilities.`,
+
+        `The movies Ocean\'s Eleven and 
+        Conquest of the Planet of the Apes
+        were filmed on UCI campus in 2001.`,
+
+        `The Ayala Science Library on UCI campus
+        is modeled after the USS Enterprise
+        from Star Trek.`
+    ]
+
+}
