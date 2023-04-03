58186928ede635b89a326313221694247efd8c56
Bibaswan Bhawal <bibaswan3600@gmail.com>
2023-04-03 06:07:30 -0700

    get languages api

--- src/api/general/general.controller.js
+++ src/api/general/general.controller.js
@@ -1,5 +1,14 @@
 import Surreal from 'surrealdb.js';
 
+/**
+ * @api {get} /api/general/languages
+ * @apiName GetLanguages
+ * @apiDescription Get a list of all languages
+ * @apiVersion 1.0.0
+ * 
+ * @apiSuccess {Object[]} languages List of languages.
+\ * @apiSuccess {[string]} languages.name Language name.
+ */
 export async function getLanguages(req, res) {
     console.time('API - getLanguages');
 
--- src/api/general/general.route.js
+++ src/api/general/general.route.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import { getLanguages } from './general.controller';
 
 const router = express.Router();
 
+router.get('/app/getLanguages', getLanguages);
 
 export default router;
